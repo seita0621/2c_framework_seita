f3db7b1bdff46ee93df449e1d68516221cee26f2
seita0621 <t.seita1234@gmail.com>
2023-11-14 12:25:02 +0900

    商品一覧の実装 #3

---
+++ MyGitProject2c/src/main/java/com/example/demo/controller/gitItemListController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class gitItemListController {
+
+
+	@RequestMapping(path = "/gititemlist", method = RequestMethod.GET)
+	public String viewPage() {
+
+		return "gititemlist";
+	}
+
+	
+}
